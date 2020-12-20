9a0da7c2f1b0cb2a2914132ff9168c8500aad8d7
Damai Yulianti Siddik <76140727+damai-code@users.noreply.github.com>
2020-12-20 18:55:14 +0700

    mengurutkan letter string a-z

---
+++ latihan-mandiri.js
@@ -0,0 +1,8 @@
+function sortCharacteristic(str) {
+return str.split('').sort().join('');
+  }
+
+console.log(sortCharacteristic("hello"));
+console.log(sortCharacteristic("truncate"));
+console.log(sortCharacteristic("developer"));
+            
